53fb097b2aae454dc9eacf74f48843010a3bb2d6
alafyte <xleb.xlebnyj@mail.ru>
2023-09-11 15:02:50 +0300

    Лабораторная работа №2

--- Lab02/lab02/02-03.js
+++ Lab02/lab02/02-03.js
@@ -2,12 +2,11 @@ let http = require('http');
 
 http.createServer((req, res) => {
     if (req.url === "/api/name" && req.method === 'GET') {
-        res.writeHead("Content-Type", "text/plain; charset=utf-8;");
+        res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8;"});
         res.end("Русак Наталья Александровна");
     } else {
-        res.writeHead("Content-Type", "text/html; charset=utf-8;");
-        res.write("<h2>Not found</h2>");
-        res.end();
+        res.writeHead(404, {"Content-Type": "text/html; charset=utf-8;"});
+        res.end("<h2>Not found</h2>");
     }
 }).listen(5000, () => console.log("Server is running at http://localhost:5000"));
 
--- Lab02/lab02/02-04.js
+++ Lab02/lab02/02-04.js
@@ -18,6 +18,9 @@ http.createServer((req, res) => {
     else if (req.url === "/api/name") {
         res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8;"});
         res.end("Русак Наталья Александровна");
+    } else {
+        res.writeHead(404, {"Content-Type": "text/html; charset=utf-8;"});
+        res.end("<h2>Not found</h2>");
     }
 
 }).listen(5000, () => console.log("Server is running at http://localhost:5000"));
